d84e257f64560afee29cacbbd925fd944605762a
jagan-pocha <pochajagan@gmail.com>
2024-10-20 12:14:54 -0600

    Added closure definition hook

--- src/hooks/DeBouncing.jsx
+++ src/hooks/DeBouncing.jsx
@@ -3,8 +3,12 @@ import './DeBouncing.css'
 const DeBouncing=()=>{
 
     const fetchDetails=(arg)=>{
+        //Every time we call fetchDetails a new timer is created 
         let timer;
         return function(){
+            // This inner function remembers the old timer even the fetch Details 
+            // is removed from memory stack, that is the concept of closure Here
+            // closure can remember outer variables 
             clearTimeout(timer);
             timer=setTimeout(()=>console.log("Searching results for : ", arg),1000);
         }
